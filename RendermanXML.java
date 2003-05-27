bdfd28ea7af002c4b7ba4cc6314277a762a73c1c
mgs <mgs@b966f6ff-49ec-0310-a7d1-893131e211bc>
2003-05-27 16:26:15 +0000

    Start
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/trunk@6 b966f6ff-49ec-0310-a7d1-893131e211bc

---
+++ RendermanXML.java
@@ -0,0 +1,16 @@
+//
+//  RendermanXML.java
+//  RendermanXML
+//
+//  Created by Olivier Van Acker on Tue May 27 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+import java.util.*;
+
+public class RendermanXML {
+
+    public static void main (String args[]) {
+        // insert code here...
+        System.out.println("Hello World!");
+    }
+}
