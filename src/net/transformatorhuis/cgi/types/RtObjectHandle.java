6ee0f90b6863d241f8ee174c233139f8aea992dc
mgs <mgs@b966f6ff-49ec-0310-a7d1-893131e211bc>
2003-12-25 11:44:39 +0000

    Added package line
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/trunk@14 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/net/transformatorhuis/cgi/types/RtObjectHandle.java
+++ src/net/transformatorhuis/cgi/types/RtObjectHandle.java
@@ -5,6 +5,7 @@
 //  Created by Olivier Van Acker on Thu Dec 25 2003.
 //  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
 //
+package net.transformatorhuis.cgi.types;
 
 public class RtObjectHandle {
 
--- src/net/transformatorhuis/cgi/types/RtToken.java
+++ src/net/transformatorhuis/cgi/types/RtToken.java
@@ -6,6 +6,8 @@
 //  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
 //
 
+package net.transformatorhuis.cgi.types;
+
 public class RtToken {
 
 }
