986d714010e4544a152c6475b77b614ca215ca6f
mgs <mgs@b966f6ff-49ec-0310-a7d1-893131e211bc>
2003-12-25 08:49:29 +0000

    Added types needed by Renderman Interface
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/trunk@12 b966f6ff-49ec-0310-a7d1-893131e211bc

---
+++ src/net/transformatorhuis/cgi/types/RtBound.java
@@ -0,0 +1,11 @@
+//
+//  RtBound.java
+//  RendermanXML
+//
+//  Created by Olivier Van Acker on Thu Dec 25 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+public class RtBound {
+
+}
