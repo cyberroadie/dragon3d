3fc3c1482853ac4e0b2857c234b0fec264944108
mgs <mgs@b966f6ff-49ec-0310-a7d1-893131e211bc>
2003-12-27 18:18:48 +0000

    - Added License notice - Added inheritance to Rib - Added constructor
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/trunk@31 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/net/transformatorhuis/cgi/conversion/Rib.java
+++ src/net/transformatorhuis/cgi/conversion/Rib.java
@@ -1,27 +1,49 @@
+/* 
+   +----------------------------------------------------------------------+
+   | RendermanXML                                                         |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2004 Cyberroadie                                       |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2 of the GNU General Public   |
+   | License, that is bundled with this package in the file LICENSE.      |
+   |                                                                      |
+   | This program is free software; you can redistribute it and/or modify |
+   | it under the terms of the GNU General Public License as published by |
+   | the Free Software Foundation; either version 2 of the License, or    |
+   | (at your option) any later version.                                  |
+   |                                                                      |
+   | This program is distributed in the hope that it will be useful,      |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+   | GNU General Public License for more details.                         |
+   |                                                                      |
+   | You should have received a copy of the GNU General Public License    |
+   | along with this program; if not, write to the Free Software          |
+   | Foundation, Inc., 59 Temple Place, Suite 330, Boston,                |
+   | MA  02111-1307  USA                                                  |
+   |                                                                      |
+   +----------------------------------------------------------------------+
+   | Author: Olivier Van Acker <cyberroadie@yahoo.co.uk>                  |
+   +----------------------------------------------------------------------+
+*/ 
 package net.transformatorhuis.cgi.conversion;
 
-//
-//  Rib.java
-//  RendermanXML
-//
-//  Created by Olivier Van Acker on Wed Dec 24 2003.
-//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
-//
+public class Rib {
 
-public abstract class Rib {
-
-    private String riName;
+    protected String element;
+    protected String paramLine;
     
-    public Rib(String ribElement, String riName) {
-        this.riName = riName;
+    public Rib(String line) {
+        int paramIndex = line.indexOf(' ');
+        element =  line.substring(0, paramIndex - 1); // -1 without the space
+        paramLine = line.substring(paramIndex + 1);
     }
     
-    public String getElement() {
-        return "<" + riName + setAttributes() + ">" + setBody() + "</" + riName + ">";
+    public void output() {
+        System.out.println(element + ": " + paramLine);
     }
 
-    public abstract String setAttributes();
-    
-    public abstract String setBody();
+    //public abstract String setAttributes();
+    //public abstract String setBody();
     
 }
