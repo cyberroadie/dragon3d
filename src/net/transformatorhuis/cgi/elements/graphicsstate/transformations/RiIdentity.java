0af700676936fef19759e1afaa493ea71e68222b
mgs <mgs@b966f6ff-49ec-0310-a7d1-893131e211bc>
2004-01-20 20:34:41 +0000

    New package structure
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/trunk@42 b966f6ff-49ec-0310-a7d1-893131e211bc

---
+++ src/net/transformatorhuis/cgi/elements/graphicsstate/transformations/RiIdentity.java
@@ -0,0 +1,43 @@
+/* 
+   +----------------------------------------------------------------------+
+   | RendermanXML                                                         |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2004 Cyberroadie                                       |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2 of the GNU General Public   |
+   | License, that is bundled with this package in the file LICENSE.      |
+   |                                                                      |
+   | This program is free software; you can redistribute it and/or modify |
+   | it under the terms of the GNU General Public License as published by |
+   | the Free Software Foundation; either version 2 of the License, or    |
+   | (at your option) any later version.                                  |
+   |                                                                      |
+   | This program is distributed in the hope that it will be useful,      |
+   | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+   | GNU General Public License for more details.                         |
+   |                                                                      |
+   | You should have received a copy of the GNU General Public License    |
+   | along with this program; if not, write to the Free Software          |
+   | Foundation, Inc., 59 Temple Place, Suite 330, Boston,                |
+   | MA  02111-1307  USA                                                  |
+   |                                                                      |
+   +----------------------------------------------------------------------+
+   | Author: Olivier Van Acker <cyberroadie@yahoo.co.uk>                  |
+   +----------------------------------------------------------------------+
+*/
+package net.transformatorhuis.cgi.elements.graphicsstate.transformations;
+
+import net.transformatorhuis.cgi.conversion.Rib;
+
+public class RiIdentity extends Rib {
+
+    public RiIdentity(String element) {
+        super(element);
+    }
+
+    public RiIdentity(String element, String param) {
+        super(element, param);
+    }
+
+}
