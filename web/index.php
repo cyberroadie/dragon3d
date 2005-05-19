2dc05a7ee1aa98f067af38e8ac7de1352792b2f0
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2005-05-19 22:58:50 +0000

    Initial version
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@99 b966f6ff-49ec-0310-a7d1-893131e211bc

---
+++ web/index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Created on May 18, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+
+<html>
+<head>
+<meta http-equiv="Content-Language" content="en" />
+<meta name="GENERATOR" content="PHPEclipse 1.0" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Dragon3D</title>
+</head>
+<body bgcolor="#FFFFFF" text="#000000" link="#FF9966" vlink="#FF9966" alink="#FFCC99">
+Dragon3D Homepage
+</body>
+</html>
+  
