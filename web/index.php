b7b2157d226961ff266b437579b7b6822708c266
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2005-05-21 21:15:56 +0000

    Initial design
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@104 b966f6ff-49ec-0310-a7d1-893131e211bc

--- web/index.php
+++ web/index.php
@@ -10,12 +10,42 @@
 <html>
 <head>
 <meta http-equiv="Content-Language" content="en" />
-<meta name="GENERATOR" content="PHPEclipse 1.0" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css" href="css/default.css" />
 <title>Dragon3D</title>
 </head>
-<body bgcolor="#FFFFFF" text="#000000" link="#FF9966" vlink="#FF9966" alink="#FFCC99">
-Dragon3D Homepage
+<body>
+	<div class="header"><img src="images/dragon3d.png" border="0" alt="Dragon 3D Project" /></div>
+	<div class="navigation">
+		<a class="navigation" href="index.php">About</a>&nbsp;&nbsp;::&nbsp;&nbsp;
+        <a class="navigation" href="news.php">News</a>&nbsp;&nbsp;::&nbsp;&nbsp;
+        <a class="navigation" href="dragon3d.php">Dragon3D</a>&nbsp;&nbsp;::&nbsp;&nbsp;
+        <a class="navigation" href="contact.php">Contact</a>
+    </div>
+    <div class="body" style="padding:20px 20px 20px 20px;">
+    	<table cellpadding="3" cellspacing="0" border="0" style="width:75%;">
+			<tr>	
+				<th style="border:1px solid #cccccc; background-color:#eeeeee; font-weight:bold;padding-left:10px;text-align:left;">About</th>
+			</tr>
+			<tr>
+				<td style="border-bottom:1px solid #cccccc; border-left:1px solid #cccccc; border-right:1px solid #cccccc;">
+				Dragon3D is a Java framework for the <a href="http://www.pixar.com/renderman/">Renderman (c) Interface</a>. 
+				Every RIB element is defined in it's own object and is created in a <a href="http://en.wikipedia.org/wiki/Factory_method_pattern">factory</a> once it is read from a RIB input file.
+				<br/>
+				The first functional implementation of this framework is a Renderman (c) Interface Bytestream file to a Renderman (c) XML format convertor.
+				</td>
+			</tr>
+		</table>
+		<br/>
+    </div>
+    <div class="footer">
+		<table cellpadding="0" cellspacing="0" border="0" style="width:100%;">
+			<tr>
+				<td style="padding-left:10px;">&copy;2005 cyberroadie</td>
+			</tr>
+		</table>
+	</div>
+	<img style="position:absolute; top:5px; right:10px;" src="images/icondragon3d.png" alt="icon" border="0" />
 </body>
 </html>
   
