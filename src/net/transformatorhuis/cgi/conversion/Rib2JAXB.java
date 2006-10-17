a3cba2acc6ac1b1f42d8501b5f479b90a2784500
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2006-10-17 21:33:53 +0000

    rib2jaxb stub added
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@187 b966f6ff-49ec-0310-a7d1-893131e211bc

---
+++ src/net/transformatorhuis/cgi/conversion/Rib2JAXB.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.transformatorhuis.cgi.conversion;
+
+/**
+ * @author cyberroadie
+ *
+ */
+public class Rib2JAXB {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
