a2cb2228fd81f326afda6a0f903301a70f8ad34a
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2007-01-28 00:17:33 +0000

    Cleanup source code.
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@249 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/main/java/net/transformatorhuis/cgi/elements/external/RiArchiveRecord.java
+++ src/main/java/net/transformatorhuis/cgi/elements/external/RiArchiveRecord.java
@@ -1,1 +1,32 @@
-/*    +----------------------------------------------------------------------+   | RendermanXML                                                         |   +----------------------------------------------------------------------+   | Copyright (c) 2004 Cyberroadie                                       |   +----------------------------------------------------------------------+   | This source file is subject to version 2 of the GNU General Public   |   | License, that is bundled with this package in the file LICENSE.      |   |                                                                      |   | This program is free software; you can redistribute it and/or modify |   | it under the terms of the GNU General Public License as published by |   | the Free Software Foundation; either version 2 of the License, or    |   | (at your option) any later version.                                  |   |                                                                      |   | This program is distributed in the hope that it will be useful,      |   | but WITHOUT ANY WARRANTY; without even the implied warranty of       |   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |   | GNU General Public License for more details.                         |   |                                                                      |   | You should have received a copy of the GNU General Public License    |   | along with this program; if not, write to the Free Software          |   | Foundation, Inc., 59 Temple Place, Suite 330, Boston,                |   | MA  02111-1307  USA                                                  |   |                                                                      |   +----------------------------------------------------------------------+   | Author: Olivier Van Acker <cyberroadie@yahoo.co.uk>                  |   +----------------------------------------------------------------------+*/package net.transformatorhuis.cgi.elements.external;import org.apache.log4j.Logger;import org.apache.log4j.BasicConfigurator;import net.transformatorhuis.cgi.conversion.Rib;public class RiArchiveRecord extends Rib {	static Logger logger = Logger.getLogger(RiArchiveRecord.class);	public RiArchiveRecord() {	    super();	}		public RiArchiveRecord(String param) {	    super(param);	}		}
+package net.transformatorhuis.cgi.elements.external;
+
+import org.apache.log4j.Logger;
+import net.transformatorhuis.cgi.conversion.Rib;
+
+/**
+ * @author cyberroadie
+ *
+ */
+public class RiArchiveRecord extends Rib {
+
+    /**
+     * Logger.
+     */
+    private static Logger logger = Logger.getLogger(RiArchiveRecord.class);
+
+    /**
+     * ArchiveRecord.
+     */
+    public RiArchiveRecord() {
+        super();
+    }
+    
+    /**
+     * ArchiveRecord.
+     * @param param
+     */
+    public RiArchiveRecord(final String param) {
+        super(param);
+    }		
+
+}
