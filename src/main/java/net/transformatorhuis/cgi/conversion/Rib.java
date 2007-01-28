63be4846b4b2820464eff72f0efbec4ea6839af5
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2007-01-28 15:43:39 +0000

    Cleanup source code.
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@269 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/main/java/net/transformatorhuis/cgi/conversion/Rib.java
+++ src/main/java/net/transformatorhuis/cgi/conversion/Rib.java
@@ -11,103 +11,109 @@ import java.util.Vector;
 
 /**
  * @author Olivier VAn Acker
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * TODO To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Style - Code Templates
  */
 public abstract class Rib {
 
     private static Logger logger = Logger.getLogger(Rib.class);
 
-    // This pattern is not perfect. 
+    // This pattern is not perfect.
     /**
      * Regular expression pattern.
      */
-    private Pattern p = Pattern.compile("\"[\\w\\s.]*\"|\\[[\\p{Graph}\\p{Blank}]*\\]|[\\d\\p{Punct}]*'");
-	
+    private Pattern p = Pattern
+            .compile("\"[\\w\\s.]*\"|\\[[\\p{Graph}\\p{Blank}]*\\]|[\\d\\p{Punct}]*'");
+
     /**
      * Paramters.
      */
     private String param = null;
-    
+
     public Rib() {
         this(null);
     }
-    
+
     public Rib(String param) {
         this.param = param;
-        
+
     }
-    
+
     /**
-     * Return name of element based on class name 
-     * without starting with 'Ri' and ending with 'Begin'
-     * and in lower case.
+     * Return name of element based on class name without starting with 'Ri' and
+     * ending with 'Begin' and in lower case.
+     * 
      * @return name of the element in lowercase
      */
     protected String getElementName() {
-    
+
         String className = this.getClass().getName();
-        String elementName = className.substring(className.lastIndexOf(".Ri") + 3, className.length());
-        if(hasChildElements()) {
-            elementName = elementName.substring(0, elementName.indexOf("Begin"));
+        String elementName = className.substring(
+                className.lastIndexOf(".Ri") + 3, className.length());
+        if (hasChildElements()) {
+            elementName = elementName
+                    .substring(0, elementName.indexOf("Begin"));
         }
         logger.debug("Element name: " + elementName);
         return elementName.toLowerCase();
-    
+
     }
-    
+
     /**
-     * Creates xml node.
-     * Most straight forward implementation, 
-     * this class is usually overwritten by sub class
-     * @param ribDoc - DOM xml document to add the xml to
+     * Creates xml node. Most straight forward implementation, this class is
+     * usually overwritten by sub class
+     * 
+     * @param ribDoc -
+     *            DOM xml document to add the xml to
      * @return xml piece for this element
      */
     public Node createXML(RibDocument ribDoc) {
-    
+
         Element ribRoot = ribDoc.requestElement(getElementName());
-        if(param != null) {
+        if (param != null) {
             ribRoot.appendChild(ribDoc.requestTextNode(param));
         }
-        
+
         return ribRoot;
-            
+
     }
 
     public boolean hasChildElements() {
         return false;
     }
-    
+
     public boolean isEndElement() {
         return false;
     }
-    
+
     /**
      * Split up parameters and put them in a Vector
-     * @param parameters - line with all parameters
+     * 
+     * @param parameters -
+     *            line with all parameters
      * @return vector with all parameters seperate
      */
     public Vector splitParameters(String parameters) {
-    	Vector parameterList = new Vector();
-    	Matcher m = p.matcher(parameters);
-    	
-    	while(m.find()) {
-    		if(m.start() != m.end()) {
-    			String match = m.group();
-    			logger.debug("Parameter: " + match);
-    			// Remove quotes
-    			if(match.startsWith("\"") && match.endsWith("\"")) {
-    				match = match.substring(1, match.length() - 1);
-    			} 
-    			parameterList.add(match);
-    		}
-    	}
-    	
+        Vector parameterList = new Vector();
+        Matcher m = p.matcher(parameters);
+
+        while (m.find()) {
+            if (m.start() != m.end()) {
+                String match = m.group();
+                logger.debug("Parameter: " + match);
+                // Remove quotes
+                if (match.startsWith("\"") && match.endsWith("\"")) {
+                    match = match.substring(1, match.length() - 1);
+                }
+                parameterList.add(match);
+            }
+        }
+
         return parameterList;
     }
-    
+
     public void setParameters(Vector parameterList, int start) {
-    	
+
     }
 }
