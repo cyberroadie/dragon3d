79a007f53f19e4e3aa430ea44fb6a23dae292036
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2007-01-28 16:46:27 +0000

    Cleanup source code.
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@290 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/main/java/net/transformatorhuis/cgi/elements/graphicsstate/options/RiDisplay.java
+++ src/main/java/net/transformatorhuis/cgi/elements/graphicsstate/options/RiDisplay.java
@@ -13,123 +13,144 @@ import org.w3c.dom.Node;
 
 /**
  * @author cyberroadie
- *
+ * 
  */
 public class RiDisplay extends Rib {
 
     static Logger logger = Logger.getLogger(RiDisplay.class);
-     
+
     private String name;
+
     private String type;
+
     private String mode;
-    
+
     private Vector parameterList;
-	
+
     public RiDisplay(String parameters) {
-    	super(parameters);
-    	parameterList = splitParameters(parameters);
-    	
+        super(parameters);
+        parameterList = splitParameters(parameters);
+
         setName((String) parameterList.get(0));
         setType((String) parameterList.get(1));
         setMode((String) parameterList.get(2));
-        if(parameterList.size() > 3) {
-        	setParameters(parameterList, 3);
+        if (parameterList.size() > 3) {
+            setParameters(parameterList, 3);
         } else {
-        	setParameters(null, 0);
+            setParameters(null, 0);
         }
     }
-    
+
     /**
      * Sets the name of picture file or framebuffer depending on type
-     * @param name  picture file or framebuffer name
+     * 
+     * @param name
+     *            picture file or framebuffer name
      */
     public void setName(String name) {
-    	this.name = name;
+        this.name = name;
     }
-    
+
     /**
      * Sets the display format, output device or output driver.
-     * @param type display type
+     * 
+     * @param type
+     *            display type
      */
     public void setType(String type) {
-    	this.type = type;
+        this.type = type;
     }
-    
+
     /**
-     * Sets the mode which indicates what data are to be output in this display stream
-     * @param mode data mode
+     * Sets the mode which indicates what data are to be output in this display
+     * stream
+     * 
+     * @param mode
+     *            data mode
      */
     public void setMode(String mode) {
-    	this.mode = mode;
+        this.mode = mode;
     }
-    
+
     /**
      * Returns the name of the picture file or framebuffer
+     * 
      * @return the name
      */
     public String getName() {
-    	return name;
+        return name;
     }
-    
+
     /**
      * Returns the display format, output device or output driver.
+     * 
      * @return the type
      */
     public String getType() {
-    	return type;
+        return type;
     }
-    
+
     /**
-     * Returns the mode which indicates what data are to be output in this display stream
+     * Returns the mode which indicates what data are to be output in this
+     * display stream
+     * 
      * @return the data mode
      */
     public String getMode() {
-    	return mode;
+        return mode;
     }
-    
+
     /**
      * Creates the XML fragment for RiDisplay rib element.
      * <p>
      * DTD fragment
+     * 
      * <pre>
-     * &lt;!ELEMENT DISPLAY - O EMPTY -- RiDisplay --&gt;
-     * &lt;!ATTLIST DISPLAY 
-     *   name    CDATA   #REQUIRED -- name of the picture file or framebuffer --
-     *   type    CDATA   #REQUIRED -- the display format, output device or output driver --
-     *   mode    CDATA   #REQUIRED -- the mode which indicates what data are to be output in this display stream --
-     * &gt;
+     *  &lt;!ELEMENT DISPLAY - O EMPTY -- RiDisplay --&gt;
+     *  &lt;!ATTLIST DISPLAY 
+     *    name    CDATA   #REQUIRED -- name of the picture file or framebuffer --
+     *    type    CDATA   #REQUIRED -- the display format, output device or output driver --
+     *    mode    CDATA   #REQUIRED -- the mode which indicates what data are to be output in this display stream --
+     *  &gt;
      * </pre>
+     * 
      * <p>
-	 * <h1>Example</h1>
-	 * Input:
-	 * <pre>
-	 * Display "swordMesh.tif" "file" "rgba"
-	 * </pre>
-	 * <p>
-	 * Output with parameters:
-	 * <pre>
-	 * &lt;display name="swordMesh.tif" type="file" mode="rgba" &gt;
-	 *	&lt;param name="[name]" value="[value]" /&gt;
-	 * &lt;/display&gt;
-	 * </pre>
-	 * <p>
-	 * Output without parameters:
-	 * <pre>
-	 * &lt;display name="swordMesh.tif" type="file" mode="rgba" /&gt;
-	 * </pre>
-	 * 
-     * @param ribDoc - DOM XML document to request an element from
+     * <h1>Example</h1>
+     * Input:
+     * 
+     * <pre>
+     *  Display &quot;swordMesh.tif&quot; &quot;file&quot; &quot;rgba&quot;
+     * </pre>
+     * 
+     * <p>
+     * Output with parameters:
+     * 
+     * <pre>
+     *  &lt;display name=&quot;swordMesh.tif&quot; type=&quot;file&quot; mode=&quot;rgba&quot; &gt;
+     * 	&lt;param name=&quot;[name]&quot; value=&quot;[value]&quot; /&gt;
+     *  &lt;/display&gt;
+     * </pre>
+     * 
+     * <p>
+     * Output without parameters:
+     * 
+     * <pre>
+     *  &lt;display name=&quot;swordMesh.tif&quot; type=&quot;file&quot; mode=&quot;rgba&quot; /&gt;
+     * </pre>
+     * 
+     * @param ribDoc -
+     *            DOM XML document to request an element from
      * @return Display xml node
      */
     public Node createXML(RibDocument ribDoc) {
-    
+
         Element ribRoot = ribDoc.requestElement(getElementName());
         ribRoot.setAttribute("name", getName());
         ribRoot.setAttribute("type", getType());
         ribRoot.setAttribute("mode", getMode());
-        
+
         return (Node) ribRoot;
-        
-    } 
+
+    }
 
 }
