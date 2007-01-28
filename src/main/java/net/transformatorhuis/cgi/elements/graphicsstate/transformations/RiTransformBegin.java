9df9cced20ad58f4cafc2002dc0a3aaf467ff92e
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2007-01-28 16:38:04 +0000

    Cleanup source code.
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@283 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/main/java/net/transformatorhuis/cgi/elements/graphicsstate/transformations/RiTransformBegin.java
+++ src/main/java/net/transformatorhuis/cgi/elements/graphicsstate/transformations/RiTransformBegin.java
@@ -33,8 +33,8 @@ public class RiTransformBegin extends Rib {
      * Child elements.
      * @return Has child elements
      */
-    public boolean hasChildElements() {
+    public final boolean hasChildElements() {
         return true;
     }
-    
+
 }
