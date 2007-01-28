5ffa50fc332ac6bb3b0864f46ab280c635ae83a8
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2007-01-28 16:13:17 +0000

    Cleanup source code.
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@277 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/main/java/net/transformatorhuis/cgi/conversion/RibFactory.java
+++ src/main/java/net/transformatorhuis/cgi/conversion/RibFactory.java
@@ -2,12 +2,12 @@ package net.transformatorhuis.cgi.conversion;
 
 import org.apache.log4j.Logger;
 import net.transformatorhuis.cgi.utils.Config;
-import java.lang.reflect.*;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Hashtable;
 
 /**
  * @author cyberroadie
- *
+ * 
  */
 public class RibFactory {
 
@@ -20,21 +20,27 @@ public class RibFactory {
      * Classes.
      */
     private Hashtable classes;
-    
-    /* All names of rib classes are in config */
-    public RibFactory(Config config) {
-        
+
+    /**
+     * Factory. All names of rib classes are in config.
+     * 
+     * @param config
+     *            configuration
+     */
+    public RibFactory(final Config config) {
+
         classes = config.getClasses();
-    
+
     }
 
     /**
      * This processes a rib element.
+     * 
      * @param ribLine
      */
-    public void processRibElement(String ribLine) {
-    
-    	logger.debug("Factory input: " + ribLine);
+    public void processRibElement(final String ribLine) {
+
+        logger.debug("Factory input: " + ribLine);
         String element = null;
         String param;
         Class ribElement;
@@ -42,32 +48,37 @@ public class RibFactory {
         Object[] intArgs;
         Class ribElementDefinition;
         Rib rib = null;
-        
+
         int c = ribLine.indexOf(' ');
-        if(c != -1) {
+        if (c != -1) {
             // With params
-            param = ribLine.substring(c, ribLine.length()); // Get rid of the EOL ('\r')
+            param = ribLine.substring(c, ribLine.length()); // Get rid of the
+                                                            // EOL ('\r')
             element = ribLine.substring(0, c);
-            intArgsClass = new Class[] {String.class}; 
-            intArgs = new Object[] {param};
+            intArgsClass = new Class[] { String.class };
+            intArgs = new Object[] { param };
 
         } else {
             // No params
-            element = ribLine; //.substring(0, ribLine.length()); // Get rid of the EOL ('\r')
-            intArgsClass = new Class[] {}; 
+            element = ribLine; // .substring(0, ribLine.length()); // Get rid
+                                // of the EOL ('\r')
+            intArgsClass = new Class[] {};
             intArgs = null;
         }
-             
-        try {   
-            ribElementDefinition = Class.forName((String) classes.get(element), true, this.getClass().getClassLoader());
-            
+
+        try {
+            ribElementDefinition = Class.forName((String) classes.get(element),
+                    true, this.getClass().getClassLoader());
+
             // Instantiate the new rib element
-            rib = (Rib) ribElementDefinition.getConstructor(intArgsClass).newInstance(intArgs);
-            
-            // Give the rib element to RibDocument which knows what to do with it
+            rib = (Rib) ribElementDefinition.getConstructor(intArgsClass)
+                    .newInstance(intArgs);
+
+            // Give the rib element to RibDocument which knows what to do with
+            // it
             RibDocument ribDoc = RibDocument.newInstance();
             ribDoc.doJob(rib);
-           
+
         } catch (InstantiationException ie) {
             System.out.println(ie);
         } catch (IllegalAccessException iae) {
@@ -83,7 +94,7 @@ public class RibFactory {
         } catch (Exception ex) {
             System.out.println(ex);
         }
-        
+
     }
-    
+
 }
