323aee55b26503c10bc9fc84067565f25983dee3
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2007-04-04 23:08:56 +0000

    Hyperboloid Test
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@347 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/main/java/net/transformatorhuis/cgi/elements/primitives/RiHyperboloid.java
+++ src/main/java/net/transformatorhuis/cgi/elements/primitives/RiHyperboloid.java
@@ -2,6 +2,7 @@ package net.transformatorhuis.cgi.elements.primitives;
 
 import org.apache.log4j.Logger;
 import net.transformatorhuis.cgi.conversion.AbstractRibType;
+import net.transformatorhuis.xsd.Hyperboloid;
 
 /**
  * @author cyberroadie
@@ -13,6 +14,11 @@ public class RiHyperboloid extends AbstractRibType {
      * Logger.
      */
     private static Logger logger = Logger.getLogger(RiHyperboloid.class);
+
+    /**
+     * JAXB Hyperboloid element
+     */
+    private Hyperboloid hyperboloid;
 
     /**
      * Hyperboloid.
@@ -28,5 +34,10 @@ public class RiHyperboloid extends AbstractRibType {
     public RiHyperboloid(final String param) {
         super(param);
     }
+
+    public Object getJAXBNode() {
+            return hyperboloid;
+    }
+
 
 }
