8bcb75d06e9d8ddca6581f81893eac2c5431f1db
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2007-07-22 11:09:20 +0000

    Added more tests for Utah Teapot
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@374 b966f6ff-49ec-0310-a7d1-893131e211bc

---
+++ src/test/java/net/transformatorhuis/cgi/elements/graphicsstate/RiAttributeTest.java
@@ -0,0 +1,47 @@
+package net.transformatorhuis.cgi.elements.graphicsstate;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import net.transformatorhuis.xsd.GeneralRibTest;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+
+/**
+ * RiAttribute Tester.
+ *
+ * @author Cyberroadie
+ * @since <pre>05/13/2007</pre>
+ * @version 1.0
+ */
+public class RiAttributeTest extends GeneralRibTest {
+
+    private static Logger logger = Logger.getLogger(RiAttributeTest.class);
+
+    private String testXMLFragment = "";
+
+    private String testRIBFragment = "";
+
+    public RiAttributeTest() {
+        super();     
+    }
+
+     @Test
+     public void testGetJAXBNode() throws IOException, SAXException, ParserConfigurationException, JAXBException {
+
+         // Create DOM document
+         Document docFromString = getDOMDocument(testXMLFragment);
+         logger.info(docFromString.toString());
+
+         // Create JAXB
+         RiAttribute rib = new RiAttribute(testRIBFragment);
+         Document docFromJAXB = getDOMFromJAXB(rib.getJAXBNode());
+
+         assertTrue(compareDocuments(docFromString, docFromJAXB));
+
+     }
+    
+}
---
+++ src/test/java/net/transformatorhuis/cgi/elements/graphicsstate/transformations/RiTranslateTest.java
@@ -0,0 +1,47 @@
+package net.transformatorhuis.cgi.elements.graphicsstate.transformations;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import net.transformatorhuis.xsd.GeneralRibTest;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+
+/**
+ * RiTranslate Tester.
+ *
+ * @author Cyberroadie
+ * @since <pre>05/13/2007</pre>
+ * @version 1.0
+ */
+public class RiTranslateTest extends GeneralRibTest {
+
+    private static Logger logger = Logger.getLogger(RiTranslateTest.class);
+
+    private String testXMLFragment = "";
+
+    private String testRIBFragment = "";
+
+    public RiTranslateTest() {
+        super();     
+    }
+
+     @Test
+     public void testGetJAXBNode() throws IOException, SAXException, ParserConfigurationException, JAXBException {
+
+         // Create DOM document
+         Document docFromString = getDOMDocument(testXMLFragment);
+         logger.info(docFromString.toString());
+
+         // Create JAXB
+         RiTranslate rib = new RiTranslate(testRIBFragment);
+         Document docFromJAXB = getDOMFromJAXB(rib.getJAXBNode());
+
+         assertTrue(compareDocuments(docFromString, docFromJAXB));
+
+     }
+    
+}
