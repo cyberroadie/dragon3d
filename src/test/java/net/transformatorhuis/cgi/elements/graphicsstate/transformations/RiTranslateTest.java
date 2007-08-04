8d50cdf0f73dd34ce7fdd3479f1e8b921aa9dfa9
cyberroadie <cyberroadie@b966f6ff-49ec-0310-a7d1-893131e211bc>
2007-08-04 12:41:53 +0000

    Added RISpec tests
    git-svn-id: http://svn.berlios.de/svnroot/repos/dragon3d/Dragon3D/trunk@391 b966f6ff-49ec-0310-a7d1-893131e211bc

--- src/test/java/net/transformatorhuis/cgi/elements/graphicsstate/transformations/RiTranslateTest.java
+++ src/test/java/net/transformatorhuis/cgi/elements/graphicsstate/transformations/RiTranslateTest.java
@@ -22,28 +22,68 @@ public class RiTranslateTest extends GeneralRibTest {
 
     private static Logger logger = Logger.getLogger(RiTranslateTest.class);
 
-    private String testXMLFragment = "<translate dx=\"0\" dy=\"0\" dz=\"25\"/>";
+    private String testUtahTeapotRIBFragment1 = "Translate 0 0 25";
+    
+    private String testUtahTeapotXMLFragment1 = "<translate dx=\"0\" dy=\"0\" dz=\"25\"/>";
 
-    private String testRIBFragment = "";
+    private String testUtahTeapotRIBFragment2 = "Translate -4.3 4.2 0";
 
+    private String testUtahTeapotXMLFragment2 = "<translate dx=\"-4.3\" dy=\"4.2\" dz=\"0\"/>";
+
+    private String testRISpecRIBFragment = "Translate 0 1 0";
+
+    private String testRISpecXMLFragment = "<translate dx=\"0\" dy=\"1\" dz=\"0\"/>";
+                                                                                         
     public RiTranslateTest() {
         super();     
     }
 
      @Ignore
      @Test
-     public void testGetJAXBNode() throws IOException, SAXException, ParserConfigurationException, JAXBException {
+     public void testUtahTeapot1() throws IOException, SAXException, ParserConfigurationException, JAXBException {
 
          // Create DOM document
-         Document docFromString = getDOMDocument(testXMLFragment);
+         Document docFromString = getDOMDocument(testUtahTeapotXMLFragment1);
          logger.info(docFromString.toString());
 
          // Create JAXB
-         RiTranslate rib = new RiTranslate(testRIBFragment);
+         RiTranslate rib = new RiTranslate(testUtahTeapotRIBFragment1);
          Document docFromJAXB = getDOMFromJAXB(rib.getJAXBNode());
 
          assertTrue(compareDocuments(docFromString, docFromJAXB));
 
      }
-    
+
+    @Ignore
+    @Test
+    public void testUtahTeapot2() throws IOException, SAXException, ParserConfigurationException, JAXBException {
+
+        // Create DOM document
+        Document docFromString = getDOMDocument(testUtahTeapotXMLFragment2);
+        logger.info(docFromString.toString());
+
+        // Create JAXB
+        RiTranslate rib = new RiTranslate(testUtahTeapotRIBFragment2);
+        Document docFromJAXB = getDOMFromJAXB(rib.getJAXBNode());
+
+        assertTrue(compareDocuments(docFromString, docFromJAXB));
+
+    }
+
+    @Ignore
+    @Test
+    public void testRISpecExample() throws IOException, SAXException, ParserConfigurationException, JAXBException {
+
+        // Create DOM document
+        Document docFromString = getDOMDocument(testRISpecXMLFragment);
+        logger.info(docFromString.toString());
+
+        // Create JAXB
+        RiTranslate rib = new RiTranslate(testRISpecRIBFragment);
+        Document docFromJAXB = getDOMFromJAXB(rib.getJAXBNode());
+
+        assertTrue(compareDocuments(docFromString, docFromJAXB));
+
+    }
+
 }
