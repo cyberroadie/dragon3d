02b4801a5f8ae5d55201f166a6ca1c15763b5092
Olivier Van Acker <cyberroadie@gmail.com>
2007-08-25 14:42:53 -0700

    Added invalid RIB document exception

---
+++ src/main/java/net/transformatorhuis/cgi/conversion/jaxb/InvalidRIBDocumentException.java
@@ -0,0 +1,10 @@
+package net.transformatorhuis.cgi.conversion.jaxb;
+
+/**
+ * @author cyberroadie
+ */
+public class InvalidRIBDocumentException extends Exception {
+    public InvalidRIBDocumentException(String message) {
+        super(message);
+    }
+}
